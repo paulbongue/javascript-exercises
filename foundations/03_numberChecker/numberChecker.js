fcf895add5bd75510271b1a18a4022c1a5799f54
Pauul BONGUE <166438316+paulbongue@users.noreply.github.com>
2026-03-05 04:54:44 +0000

    MOdification des codes pour la validation de chaque test.

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,10 +1,44 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 }
+
+// function numberChecker(number) {
+//   if (number >= 1000) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function numberChecker(number) {
+//   if (number === 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function numberChecker(number) {
+//   if (number === 9) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+// function numberChecker(number) {
+//   if (number === 6) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+
 
 // Do not edit below this line
 module.exports = numberChecker;
